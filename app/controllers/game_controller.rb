bf35244b51281060c48f3e75f53155e1a5475001
Vivian Lin, Jack Fernbacher, William Tan <vivianlin.is@gmail.com>
2016-08-23 16:12:45 -0700

    second commit

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,6 @@
 class GameController < ApplicationController
 
   def hilo
-    @user_guess = params[:user_input].to_i
     random_number = 1 + rand(10)
 
     #Assigns a random number to a cookie if there is none
@@ -10,21 +9,33 @@ class GameController < ApplicationController
       cookies[:attempts] = 0
     end
 
-    #Hi-Lo game
-    if @user_guess > session[:answer].to_i
-      @message = "Your number is too high!"
-      cookies[:attempts] = cookies[:attempts].to_i + 1
-      @tries_message = cookies[:attempts]
-    elsif @user_guess < session[:answer].to_i
-      @message = "Your number is too low!"
-      cookies[:attempts] = cookies[:attempts].to_i + 1
-      @tries_message = cookies[:attempts]
+    if params[:user_input].nil?
+      flash[:alert] = "Please input a number into the field."
     else
-      @message = "Good job!"
-      session[:answer] = random_number
-      cookies[:attempts] = cookies[:attempts].to_i + 1
-      @tries_message = cookies[:attempts]
-      cookies[:attempts] = 0
+      @user_guess = params[:user_input].to_i
+    #Hi-Lo game
+      if @user_guess > session[:answer].to_i
+        @message = "Your number is too high!"
+        cookies[:attempts] = cookies[:attempts].to_i + 1
+        @tries_message = cookies[:attempts]
+      elsif @user_guess < session[:answer].to_i
+        @message = "Your number is too low!"
+        cookies[:attempts] = cookies[:attempts].to_i + 1
+        @tries_message = cookies[:attempts]
+      else
+        @message = "Good job!"
+        session[:answer] = random_number
+        cookies[:attempts] = cookies[:attempts].to_i + 1
+        @tries_message = cookies[:attempts]
+        cookies[:attempts] = 0
+      end
     end
+  end
+
+
+  def reset
+    cookies[:attempts] = 0
+    session[:answer] = nil
+    redirect_to "http://localhost:3000/game/hilo"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/game/hilo' => 'game#hilo'
+  get '/game/new_game' => 'game#reset'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
